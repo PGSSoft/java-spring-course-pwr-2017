02c6ea6ebd2b968dc64af7dc6805d856b877a255
Gvaireth <pzajaczkowski@gmail.com>
2017-03-14 12:53:49 +0100

    2

--- src/main/java/com/example/controller/WeatherForecastController.java
+++ src/main/java/com/example/controller/WeatherForecastController.java
@@ -1,4 +1,16 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/weather")
 public class WeatherForecastController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello world!";
+    }
+
+
 }
