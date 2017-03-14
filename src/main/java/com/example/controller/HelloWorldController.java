58f377272f28941b735ca0915ebd326de37076a8
Piotr Kaliniak <piotr.kaliniak@gmail.com>
2017-03-14 18:54:34 +0100

    last commit

--- src/main/java/com/example/controller/HelloWorldController.java
+++ src/main/java/com/example/controller/HelloWorldController.java
@@ -21,4 +21,6 @@ public class HelloWorldController {
     public String helloName(@PathVariable String name) {
         return "Hello " + name;
     }
+
+
 }
--- src/main/java/com/example/controller/WeatherController.java
+++ src/main/java/com/example/controller/WeatherController.java
@@ -3,10 +3,15 @@ package com.example.controller;
 import com.example.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -32,6 +37,23 @@ public class WeatherController {
         List<String> result = new ArrayList<String>();
         weatherServices.forEach(((x)->result.add(x.getWeather())));
         result.add(myBean);
+        return result;
+    }
+
+    @RequestMapping("/weather/status")
+    public ResponseEntity<List<String>> allWeathersStatus() {
+        List<String> result = new ArrayList<String>();
+        weatherServices.forEach(((x)->result.add(x.getWeather())));
+        return new ResponseEntity<List<String>>(result, HttpStatus.BAD_REQUEST);
+    }
+    @RequestMapping("/weather/status/dec")
+    public List<String> allWeathersStatusDecorator(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) {
+        List<String> result = new ArrayList<String>();
+        weatherServices.forEach(((x)->result.add(x.getWeather())));
+        response.setStatus(401);
         return result;
     }
 
