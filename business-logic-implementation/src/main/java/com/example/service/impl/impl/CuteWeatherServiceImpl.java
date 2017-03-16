4927af7284430a429c735dcc6280f293f1d9e0af
Piotr Kaliniak <piotr.kaliniak@gmail.com>
2017-03-16 17:12:48 +0100

    split into modules

--- business-logic-implementation/src/main/java/com/example/service/impl/impl/CuteWeatherServiceImpl.java
+++ business-logic-implementation/src/main/java/com/example/service/impl/impl/CuteWeatherServiceImpl.java
@@ -1,10 +1,6 @@
 package com.example.service.impl;
 
 import com.example.service.WeatherService;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 /**
